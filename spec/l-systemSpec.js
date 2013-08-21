53698035e15617fa9d045970e0873631c12fa52d
Chris Sammis <csammis@gmail.com>
2013-08-21 16:18:43 -0500

    Fleshed out spec for l-system production

--- spec/l-systemSpec.js
+++ spec/l-systemSpec.js
@@ -1,10 +1,31 @@
 describe("A set of tests for an l-system production", function() {
 
     it("Production creates OK when given lhs and rhs", function() {
-        expect(l-system.production({lhs: "A", rhs: "BA"}).toEqual(jasmine.any(Function)));
+        expect(lsystem.production({lhs: "A", rhs: "BA"})).toEqual(jasmine.any(Object));
     });
 
-    it("Can't create a production without lhs and rhs", function() {
-        expect(l-system.production({ })).toMatch(/ERROR/);
+    it("Can't create a production without both lhs and rhs", function() {
+        expect(lsystem.production({ })).toMatch(/ERROR/);
+        expect(lsystem.production({lhs: "A"})).toMatch(/ERROR/);
+        expect(lsystem.production({rhs: "A"})).toMatch(/ERROR/);
+    });
+
+    it("Can't create a production with an empty lhs", function() {
+        expect(lsystem.production({lhs: "", rhs: "BA"})).toMatch(/ERROR/);
+    });
+
+    it("Production A -> BA on A yields ABA", function() {
+        var p = lsystem.production({lhs: "A", rhs: "BA"});
+        expect(p.produce("A")).toEqual("ABA");
+    });
+
+    it("Production A -> BA on B yields B", function() {
+        var p = lsystem.production({lhs: "A", rhs: "BA"});
+        expect(p.produce("B")).toEqual("B");
+    });
+
+    it("Production A -> BA on ABBAABABA yields ABBAABABABA", function() {
+        var p = lsystem.production({lhs: "A", rhs: "BA"});
+        expect(p.produce("ABBAABABA")).toEqual("ABBAABABABA");
     });
 });

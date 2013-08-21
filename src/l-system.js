219a8490a50717218a83b8d0beb1371201527db8
Chris Sammis <csammis@gmail.com>
2013-08-21 10:05:26 -0500

    Moved l-system.js to a src/ directory

---
+++ src/l-system.js
@@ -0,0 +1,20 @@
+var l-system = {
+    // lhs: string, rhs: string
+    production : function(spec) {
+        if (spec.lhs === undefined || spec.lhs.length() == 0 || spec.rhs === undefined) {
+            return "ERROR - must specify lhs and rhs";
+        };
+
+        var that = { };
+
+        that.produce = function(input) {
+            if (input.substr(spec.lhs.length() * -1) == spec.lhs) {
+                return input.slice(0, input.length() - spec.lhs.length()) + spec.rhs; 
+            }
+
+            return input;
+        };
+
+        return that;
+    };
+};

fd4e6ed00ef049573485674fd2a15e1de95fae21
Chris Sammis <csammis@gmail.com>
2013-11-01 22:32:25 -0500

    Rendering with turtle graphics

--- src/render/turtle.js
+++ src/render/turtle.js
@@ -14,6 +14,41 @@ function TurtleRender() {
 
     this.render = function(data) {
         var $canvas = self.getRenderCanvas();
+        var context = $canvas[0].getContext('2d');
+
+        context.fillStyle = "#ffffff";
+        context.fillRect(0, 0, $canvas.width(), $canvas.height());
+
+        var currDir = self.NORTH, step = 5;
+        var x = ($canvas.width() / 2), y = ($canvas.height() / 2);
+
+        context.fillStyle = "#000000";
+        for (var i = 0; i < data.length; i++) {
+            var c = data.charAt(i);
+            switch (c)
+            {
+                case 'F':
+                    context.beginPath();
+                    context.moveTo(x, y);
+                    x += currDir.x * step;
+                    y += currDir.y * step;
+                    context.lineTo(x, y);
+                    context.stroke();
+                    break;
+                case 'L':
+                    if (currDir == self.NORTH) currDir = self.WEST;
+                    else if (currDir == self.WEST) currDir = self.SOUTH;
+                    else if (currDir == self.SOUTH) currDir = self.EAST;
+                    else if (currDir == self.EAST) currDir = self.NORTH;
+                    break;
+                case 'R':
+                    if (currDir == self.NORTH) currDir = self.EAST;
+                    else if (currDir == self.EAST) currDir = self.SOUTH;
+                    else if (currDir == self.SOUTH) currDir = self.WEST;
+                    else if (currDir == self.WEST) currDir = self.NORTH;
+                    break;
+            }
+        }
     };
 
     var self = {
@@ -30,6 +65,11 @@ function TurtleRender() {
                 $displayArea.append($obj);
             }
             return $obj;
-        }
+        },
+
+        NORTH : { x : 0, y : -1 },
+        SOUTH : { x : 0, y : 1 },
+        EAST : {x : 1, y : 0},
+        WEST : {x : -1, y : 0}
     };
 };
